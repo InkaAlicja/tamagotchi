b35b6812ab05b9e5d7d85218e270664d446f4ceb
Inka Sokolowska <inka.sokolowska@student.uj.edu.pl>
2020-06-07 23:35:21 +0200

    pong frames :)))

--- src/View/PongView.java
+++ src/View/PongView.java
@@ -67,8 +67,8 @@ public class PongView {
         score=0;
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        //window.setMinHeight(450);
-       // window.setMinWidth(600);
+        window.setMinHeight(450);
+        window.setMinWidth(600);
         window.setResizable(false);
         window.setTitle("Pong!");
 
